4bcb72b563bb3301bcf012af97a3d6577bb6a884
MrDx24 <deeppatadiya24@gmail.com>
2020-03-14 22:02:06 +0530

    forgetpasswordts code updated

--- client/src/app/forgot-password/forgot-password.component.ts
+++ client/src/app/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { compareValidator } from '../shared/compare-validator.directive';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgetPasswordForm: FormGroup;
+  psswd = null;
+  cnpsswd = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router) { }
 
   ngOnInit() {
+    this.forgetPasswordForm  = this.fb.group ({
+      emailfrgpsswd : ['', Validators.required],
+      passwordfrgpsswd : ['', Validators.required],
+      conPsswdfrgpsswd : ['', [Validators.required, compareValidator('passwordfrgpsswd')]]
+    });
+
+  }
+
+  forgotPassword() {
+
+
+    this.http.post('http://localhost:3000/user/fgpsswd', this.forgetPasswordForm.value).subscribe((response: any) => {
+
+      console.log(response);
+      alert('Password changed Successfully');
+
+    }, (error) => {
+
+      console.log(error);
+      alert('Password changing Failed');
+
+    });
+  }
+
+  onbackClick() {
+    this.router.navigate(['/login']);
+  }
+
+  get emailfrgpsswd() {
+    return this.forgetPasswordForm.get('emailfrgpsswd');
+  }
+
+  get passwordfrgpsswd() {
+    return this.forgetPasswordForm.get('passwordfrgpsswd');
+  }
+
+  get conPsswdfrgpsswd() {
+    return this.forgetPasswordForm.get('conPsswdfrgpsswd');
   }
 
 }
